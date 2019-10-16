a5cc0fdc66b810af67e3500e087fdd06e6d69604
chngo <chan.ngo2203@gmail.com>
2019-10-15 21:17:53 -0400

    Add 2 example tasks in main.c

--- src/main.c
+++ src/main.c
@@ -8,26 +8,49 @@
 #include "task.h"
 #include <avr/io.h>
 
-
-// EXAMPLE BLINKER TASK
-void blinkLED(void* parameter)
+void taskLedON(void* arguments)
 {
-	DDRB |= (1 << PB5);		// PB.5 as output
-	for (;;)
-	{
-		PORTB &= ~(1 << PB5);	// Turn LED on
-		vTaskDelay(250);	// Wait
-		PORTB |= (1 << PB5);	// Turn LED off
-		vTaskDelay(250);	// Wait
+   // ENTER TASK'S LOOP
+   while(1)
+   {
+      PORTB |= (1 << PB5);    // Turn LED on
+      vTaskDelay(500);        // Wait 500ms
 	}
+}
+
+
+void taskLedOFF(void* arguments)
+{
+   // ENTER TASK'S LOOP
+   while(1)
+   {
+      PORTB &= ~(1 << PB5);    // Turn LED off
+      vTaskDelay(250);        // Wait 250ms
+   }
 }
 
 // MAIN PROGRAM
 int main(void)
 {
-	// CREATE BLINKER TASK
-	xTaskCreate(blinkLED, "blink", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+	// CONFIGURE PORT B, PIN 5 AS OUTPUT (my LED is here)
+   DDRB |= (1 << PB5);
+	
+	// CREATE ON TASK
+   xTaskCreate(taskLedON,
+               "ON",
+               configMINIMAL_STACK_SIZE, 
+               NULL, 
+               tskIDLE_PRIORITY,
+               NULL);
 
+   // CREATE OFF TASK   
+   xTaskCreate(taskLedOFF, 
+               "OFF", 
+               configMINIMAL_STACK_SIZE, 
+               NULL,  
+               tskIDLE_PRIORITY+1, 
+               NULL);
+	
 	// START SCHELUDER
 	vTaskStartScheduler();
  
